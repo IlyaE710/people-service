59e2f54391c4538deedd9191a4b9752b8a6002fa
ilyaE <ilyaermakov710@ya.ru>
2024-01-27 00:36:03 +0300

    feat(repository): enhance repository methods with logging
    Improve the repository methods in the People repository by adding detailed logging for received requests, conversion processes, and success or failure of database operations. This enhances visibility into the repository layer and aids in debugging.

--- internal/v1/repository/people/repository.go
+++ internal/v1/repository/people/repository.go
@@ -17,12 +17,18 @@ func NewRepository(db *gorm.DB) *Repository {
 }
 
 func (r *Repository) GetByName(name string) (*dto.People, error) {
+	logrus.WithFields(logrus.Fields{
+		"Name": name,
+	}).Info("Repository - GetByName")
 	model := entity.People{}
 
 	if err := r.db.Where("name = ?", name).Preload("Country").First(&model).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
+			logrus.Warnf("Person not found by name: %s", name)
 			return nil, err
 		}
+		logrus.Errorf("Error retrieving person by name: %v", err)
+		return nil, err
 	}
 
 	result := &dto.People{
@@ -47,9 +53,11 @@ func (r *Repository) GetByName(name string) (*dto.People, error) {
 }
 
 func (r *Repository) GetAll() ([]*dto.People, error) {
+	logrus.Info("Repository - GetAll")
 	var models []entity.People
 
 	if err := r.db.Find(&models).Error; err != nil {
+		logrus.Errorf("Error retrieving all people: %v", err)
 		return nil, err
 	}
 
@@ -67,10 +75,16 @@ func (r *Repository) GetAll() ([]*dto.People, error) {
 }
 
 func (r *Repository) Delete(person dto.DeletePeople) error {
+	logrus.WithFields(logrus.Fields{
+		"ID": person.Id,
+	}).Info("Repository - Delete")
 	return r.db.Where("id = ?", person.Id).Delete(&entity.People{}).Error
 }
 
 func (r *Repository) Update(person dto.UpdatePeople) error {
+	logrus.WithFields(logrus.Fields{
+		"UpdatePeople": person,
+	}).Info("Repository - Update")
 	model := entity.People{
 		ID:         person.ID,
 		Name:       person.Name,
@@ -86,6 +100,9 @@ func (r *Repository) Update(person dto.UpdatePeople) error {
 }
 
 func (r *Repository) Add(person dto.CreatePeople) error {
+	logrus.WithFields(logrus.Fields{
+		"CreatePeople": person,
+	}).Info("Repository - Add")
 	model := entity.People{
 		Name:       person.Name,
 		Surname:    person.Surname,
